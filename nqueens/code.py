7f67e6554c2433b2796b85ad73bb15d3ae3c19ae
AMKing <TOSHIHIRORYUU96@GMAIL.COM>
2020-12-17 18:27:22 +0530

    added initial functions

--- nqueens/code.py
+++ nqueens/code.py
@@ -3,7 +3,60 @@ import subprocess
 import ast
 from collections import namedtuple
 
-def queens(n):
+
+# Route n-queens based on algorithm selected.
+def queens(n = 4, algo = "divide"):
+
     print("n-queens",n)
 
+    if algo == "divide":
+        # get no of solution possible and their position as list
+        count, pos = nqueens_branch(n)
+    else:
+        # get no of solution possible and their position as list
+        count, pos = nqueens_backtrack(n)
 
+
+# Use Branch and bound to solve the problem.
+def nqueens_branch(n):
+    # Set initial count to 0
+    count = 0
+    pos = []
+    return count, pos
+
+
+# Use Backtracking to solve the problem.
+def nqueens_backtrack(n):
+    count = 0
+    # ist to hold queen positions
+    pos = []
+    return count, pos
+
+# Print the possible soultions as 0,1 on board
+def show():
+    pass
+
+
+# Display the solution as an image
+def display():
+    pass
+
+
+# save the n quuens image
+def save():
+    pass
+
+
+# Get an image and identify the n-value and position of queens if present.
+def scan_queen():
+    pass
+
+# Solve n-queens for an image
+def image_solver():
+    pass
+
+# Return Positions and solution space as audio.
+def alexa():
+    pass
+
+queens(4, )
