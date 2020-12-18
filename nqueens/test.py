f06a2e8668ef8c379fbe64333afa955c15bc58f3
AMKing <TOSHIHIRORYUU96@GMAIL.COM>
2020-12-18 10:47:13 +0530

    Added test code for scan_queen

--- nqueens/test.py
+++ nqueens/test.py
@@ -4,12 +4,18 @@ from code import *
 # Test code
 queens(4, )
 
-queen_data=[
-    [0,1,0,0],
-    [0,0,0,1],
-    [1,0,0,0],
-    [0,0,1,0]]
+# queen_data=[
+#     [0,1,0,0],
+#     [0,0,0,1],
+#     [1,0,0,0],
+#     [0,0,1,0]]
 
-display(queen_data)
-save(queen_data, "saved_image")
-save(queen_data)
+# display(queen_data)
+# save(queen_data, "saved_image")
+# save(queen_data)
+
+image_solver("tests\q4.PNG")
+# image_solver("q4b.png")
+# image_solver("q5.png")
+# image_solver("q5b.png")
+# image_solver("q6.png")
